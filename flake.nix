da52dc50506a00472d06bd55e9693ff180b5993e
Jack Bartlett <jacbart@gmail.com>
2024-03-26 07:36:01 -0700

    style(md): markdown in welcome message changes

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
           # Welcome to the Rust project template!
 
           This template is a simple starting point for a new Rust project. It includes a Cargo.toml file with some common dependencies and a simple main.rs file that prints "Hello, world!".
-          Replace `BIN_NAME` with the name of your binary in the Cargo.toml file and the main.rs file.
-          ```sh
+          Replace `BIN_NAME` with the name of your binary in the Cargo.toml file and the main.rs file.  
+          ```sh  
           rg 'BIN_NAME' {Cargo.toml,flake.nix,.gitignore}
-          ```
-          >For more information about Rust and Cargo, check out the official documentation at https://doc.rust-lang.org/cargo/.
+          ```  
+          >For more information about Rust and Cargo, check out the official documentation at https://doc.rust-lang.org/cargo/.  
         '';
       };
       go = {
@@ -23,11 +23,11 @@
         welcomeText = ''
           # Welcome to the Golang project template!
 
-          This template is a simple starting point for a new Nix Go project. To setup the go project run the below command
-          ```sh
+          This template is a simple starting point for a new Nix Go project. To setup the go project run the below command  
+          ```sh  
           go mod init github.com/username/projectname
-          ```
-          >Edit the `flake.nix` file to rename the binary, add dependencies and build instructions.
+          ```  
+          >Edit the `flake.nix` file to rename the binary, add dependencies and build instructions.  
         '';
       };
     };
