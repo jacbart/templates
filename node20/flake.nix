61c40e8c9eb4cfee9e0b31982cdd0ef12936c25d
Jack Bartlett <jacbart@gmail.com>
2024-07-17 14:37:54 -0700

    feat(nodejs): versions 18, 20 and 22

---
+++ node20/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "nodejs version 20";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  in {
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = pkgs.mkShell {
+          name = "node20";
+          buildInputs = with pkgs; [
+            nodejs_20
+          ];
+        };
+    });
+  };
+}
